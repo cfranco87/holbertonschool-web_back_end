602738346f25012093ca183e28af67c7a250652a
root <carlosfrancoart@gmail.com>
2024-01-04 22:40:44 +0000

    pycode

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ annotated type function n"""
 
-def floor(n: float)-> int:
+
+def floor(n: float) -> int:
     """function that takes float and return int"""
     return int(n)
