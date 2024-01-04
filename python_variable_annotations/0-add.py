c5f8e5c8c07906c5347dc18a6fcaa6cef1e99ecd
root <carlosfrancoart@gmail.com>
2024-01-04 02:04:21 +0000

    annotated type add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def add (a: float, b: float) -> float:
+    """
+    first variable - a / float
+    second variable - b / float
+    Type-annotated function that takes two floats and returns the sum as float
+    """
+    
+    return a + b
