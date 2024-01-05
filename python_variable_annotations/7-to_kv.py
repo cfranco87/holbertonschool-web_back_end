e37a4cd54b2d4071dd7b5f0e7950f2f723cdfd9b
root <carlosfrancoart@gmail.com>
2024-01-05 02:20:33 +0000

    tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,4 +5,5 @@ from typing import List, Union, Tuple
 
 def to_kv(k: str, v: [Union[int, float]]) -> Tuple[str, float]:
     """ converting a string and int/float to a tuple"""
-    return (k, float(v ** 2))
+    kv = (k, v * v)
+    return kv
