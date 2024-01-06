53b83b8fc1b6d704dff399ad6bce25e786034baa
root <carlosfrancoart@gmail.com>
2024-01-06 00:21:28 +0000

    tasks in range

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,12 +10,11 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     function call wait_random n times async
     n = num times called
     max_delay = num of delay passed to wait_random
-    return list of wait time 
+    return list of wait time
     """
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     list = []
-    for task in asyncio.as_completed(tasks)
-        time : float = await task
+    for task in asyncio.as_completed(tasks):
+        time: float = await task
         list.append(time)
 return list
-    
