a40dece074d6f68e3ad3bb972b06abdbd4f82e58
root <carlosfrancoart@gmail.com>
2024-01-06 00:20:09 +0000

    tasks in range

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""function to run multiple coroutines"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    function call wait_random n times async
+    n = num times called
+    max_delay = num of delay passed to wait_random
+    return list of wait time 
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    list = []
+    for task in asyncio.as_completed(tasks)
+        time : float = await task
+        list.append(time)
+return list
+    
