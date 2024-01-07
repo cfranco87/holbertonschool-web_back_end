79abe432a8a666cb5c72f0386c4ae9e48fed0294
root <carlosfrancoart@gmail.com>
2024-01-07 01:33:52 +0000

    4 times comp

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""running 4 parellel comps"""
+import asyncio
+from time import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    start_time = time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    end_time = time()
+    total = end_time - start_time
+    return total
