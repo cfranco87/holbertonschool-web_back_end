953040c93297d84d5499c24268eab6310f5e1579
root <carlosfrancoart@gmail.com>
2024-01-07 00:59:04 +0000

    random with generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""working with async generator"""
+import asyncio
+from random import uniform
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    -generators work with iterables
+    -uniform(a, b) function is used
+    to generate a random floating-point number
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield uniform(1, 10)
---
+++ python_async_comprehension/README.md
@@ -0,0 +1,1 @@
+working with async comprehension, generators and iterables
