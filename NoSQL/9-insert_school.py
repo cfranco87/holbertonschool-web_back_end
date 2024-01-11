1fc6eb79827e159c8a1528dfbb5455cf85eb87fb
root <carlosfrancoart@gmail.com>
2024-01-11 14:55:37 -0300

    kwargs

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Python func that list all docs in collection based on kwargs"""
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    _id = will hold mongo_collection, inside mongo is kwargs
+    mongo_collection is a pymongo collection object
+    """
+     _id = mongo_collection.insert_one(kwargs).inserted_id
+    return _id
