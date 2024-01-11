e4d2e507a7f6fde11e6623d8dc07efa9ac968487
root <carlosfrancoart@gmail.com>
2024-01-11 15:18:35 -0300

    last 3

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Python func that list all docs in collection ba"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+
+
+
+
+    return _id
---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+"""Python func that list all docs in collection ba"""
+import pymongo
