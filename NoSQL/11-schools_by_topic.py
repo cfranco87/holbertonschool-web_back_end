20ac71caa6c3cfed30112f12c45ade0255451286
root <carlosfrancoart@gmail.com>
2024-01-13 03:02:34 +0000

    school topic

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
-"""Python func that list all docs in collection ba"""
+"""Returns a list of schools having a specific topic in a MongoDB collection."""
 import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    - mongo_collection: A pymongo collection object.
+    - topic: The topic to search for (string).
+    - Using the find method to query documents with the specified topic
+    """
+    folder = mongo_collection({"topics": topic})
+    
+    """ Extracting the list of schools from the cursor"""
+    school_list = [document["name"] for document in folder]
+
+    return school_list
