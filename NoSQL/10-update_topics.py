2e33c320cfb3aa233baf370dfef4c35767e0e00b
root <carlosfrancoart@gmail.com>
2024-01-13 02:51:34 +0000

    insert school

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-"""Python func that list all docs in collection ba"""
+"""Update the topics of a school in a MongoDB collection"""
 import pymongo
 
 
 def update_topics(mongo_collection, name, topics):
-
-
-
-
+    """
+    - mongo_collection: A pymongo collection object.
+    - name: The name of the school to be updated.
+    - topics: A list of topics to update for the school.
+    """
+    result = mongo_collection.update_one(
+        {"name": name}, {"$set": {"topics": topics}}
+        )
+    _id = result.upserted_id
+    
     return _id
