1b63e6df1081e7bdec93b7d205c9b48d8cb0f34b
Carlos Franco <carlosfrancoart@gmail.com>
2024-02-25 21:47:32 -0300

    all

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,17 @@
+export default function createReportObject(employeesList) {
+    const allEmployees = {};
+  
+    for (const department in employeesList) {
+  
+      if (Object.prototype.hasOwnProperty.call(employeesList, department)) {
+        allEmployees[department] = [...employeesList[department]];
+      }
+    }
+  
+    return {
+      allEmployees,
+      getNumberOfDepartments() {
+        return Object.keys(allEmployees).length;
+      },
+    };
+  }
